5e1ca4e44844505d9a5d5d7b1d2b5e7ad76f4d4e
ajbdev <andybaird@gmail.com>
2013-04-03 19:10:34 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get install vim htop ntp
 sudo apt-get install gcc g++ make libpcre3 libpcre3-dev
-sudo apt-get install php5-common php5-cgi php5 nginx php5-intl php5-mysql php-pear php5-mcrypt php5-dev php-apc mysql-server php5-memcache php5-cli php5-curl php5-gd 
+sudo apt-get install php5-common php5-cgi php5 php5-fpm nginx php5-intl php5-mysql php-pear php5-mcrypt php5-dev php-apc mysql-server php5-memcache php5-cli php5-curl php5-gd 
 sudo pear install php5-oauth
 sudo pear install php-sundown
 sudo pecl install oauth

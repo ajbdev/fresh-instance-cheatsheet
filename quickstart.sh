17fb739cd18586b3cbf422d97e36504b2a66a6af
ajbdev <andybaird@gmail.com>
2013-04-03 19:03:22 -0500

    Create quickstart.sh

---
+++ quickstart.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get install vim htop ntp
+sudo apt-get install gcc g++ make
+sudo apt-get install php5-common php5-cgi php5 nginx php5-intl php5-mysql php-pear php5-mcrypt php5-dev php-apc mysql-server php5-memcache php5-cli php5-curl php5-gd 

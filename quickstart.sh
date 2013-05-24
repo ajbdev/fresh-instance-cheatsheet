b534be9614d20f61114b17d7e8f45d69936dec7e
ajbdev <andybaird@gmail.com>
2013-05-24 16:25:23 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt-get update
 sudo apt-get -y install vim htop ntp
 sudo apt-get -y install gcc g++ make libpcre3 libpcre3-dev
 sudo apt-get -y install php5-common php5-cgi php5 php5-fpm nginx php5-intl php5-mysql php-pear php5-mcrypt php5-dev php-apc mysql-server php5-memcache php5-cli php5-curl php5-gd 
